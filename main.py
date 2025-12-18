5e36e84dba458cc0b8e54ea8e9de58ba2ee9e452
par <parsquads@gmail.com>
2025-12-17 16:55:53 -0800

    added some code

---
+++ main.py
@@ -0,0 +1,6 @@
+#with open("books/frankenstein.txt") as f:
+f_split = []
+f_split = "books/frankensein.txt".split
+print (f_split)
+f_split_length = len("books/frankensein.txt")
+print(f" found {f_split_length} total words")
